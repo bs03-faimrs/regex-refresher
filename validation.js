36a7d502910f5e6a1478cc30f411931d4f911669
bs03-faimrs <benjamin.sunday@faimrs.com>
2020-09-04 14:03:00 +0100

    Validation ecxample with regex

---
+++ validation.js
@@ -0,0 +1,33 @@
+// validation script here
+// let reg2 = new RegExp('/[a-z]/', 'i')
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,9}$/i,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+}
+
+
+// Validation function
+const validate = (field, regex) =>{
+    // console.log(regex.test(field.value));  
+    if(regex.test(field.value)){
+        field.className = "valid";
+    }else {
+        field.className = "invalid"
+    }
+}
+
+inputs.forEach( input =>{
+    input.addEventListener('keyup', e =>{
+        // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+})
+
+
+
